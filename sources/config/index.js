dffae3991e528ea40c8691c83002ba89f728a410
samipe <sami_kolari@hotmail.com>
2018-08-07 21:00:50 +0300

    Update index.js

--- sources/config/index.js
+++ sources/config/index.js
@@ -57,7 +57,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../../'),
     assetsSubDirectory: 'static',
 
-    assetsPublicPath: '/',
+    assetsPublicPath: '/robocon',
 
     /**
      * Source Maps
