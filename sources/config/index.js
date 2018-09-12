c9640cf0d6483409a3b4f38eb3c22f1149723157
Antti Karjalainen <antti.karjalainen@siili.com>
2018-09-12 21:29:52 +0300

    Change assetsPublicPath

--- sources/config/index.js
+++ sources/config/index.js
@@ -57,7 +57,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../../'),
     assetsSubDirectory: 'static',
 
-    assetsPublicPath: '/robocon',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
