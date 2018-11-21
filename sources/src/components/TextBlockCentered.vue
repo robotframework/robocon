a4e71de45f63b83422f5f18f89ebc015c28b8ddf
Sami Kolari <sami.kolari@siilim180518.local>
2018-11-21 10:48:22 +0200

    twitter feed + sponsor images fixed

--- sources/src/components/TextBlockCentered.vue
+++ sources/src/components/TextBlockCentered.vue
@@ -1,38 +1,53 @@
 <template>
-  <div class="col-lg-8 col-md-10 mx-auto">
+<div
+    class="container">
+    <div v-if="data.twitter === false" class="col-lg-12 col-md-12 mx-auto">
     <div class="row-md-5 row-lg-4 order-1 textblock-left no-border dashed-header link-fix">
       <h1 v-html="data.header"/>
     </div>
      <div v-html="data.text" class="row order-2 block-text textblock-right p-3 no-border text-justify"/>
-    <div v-if="data.src" class="row order-2 textblock-right text-justify no-border p-3">
-     <b-embed type="iframe"
-           aspect="16by9"
-           :src="data.src"
-           allowfullscreen
-      ></b-embed>
-    </div>
-
-     <div v-if="data.twitter == true" class="col-md-12 col-lg-3 order-3 textblock-right ml-1 p-3">
-       <twitter-timeline class="p-xs-5 p-lg-0" id="robotframework" widget-class="twitterprofile" sourceType="profile" :options="options"/>
-     </div>
   </div>
+  <div
+    v-else>
+    <div class="col-lg-12 mx-auto">
+    <div class="row-md-5 row-lg-4 order-1 textblock-left no-border dashed-header link-fix">
+      <h1 v-html="data.header"/>
+    </div>
+    <div
+        class="row">
+        <div v-html="data.text" class="col-lg-7 order-2 block-text textblock-right p-3 no-border text-justify"/>
+     <div class="col-md-12 col-lg-4 order-3 textblock-right ml-1 p-3">
+       <twitter-timeline class="p-xs-5 p-lg-0" id="robotframework" widget-class="twitterprofile" sourceType="profile" :style="isMobile ? '' : isTablet ? 'height: 850px; overflow: scroll' : 'height: 700px; overflow: scroll'" :options="options"/>
+     </div>
+        </div>
+  </div>
+    </div>
+    </div>
 </template>
 
 <script>
-import Timeline from "vue-tweet-embed/timeline";
+import Timeline from "vue-tweet-embed/timeline"
 export default {
   props: ["data"],
   components: {
     "twitter-timeline": Timeline
   },
-  data() {
+  data () {
     return {
       options: {
-        theme: "light",
-        tweetLimit: "3",
-        chrome: ["nofooter", "noscrollbar"]
+        theme: "dark",
+        tweetLimit: (window.innerWidth < 992) ? 3 : 10,
+        chrome: (window.innerWidth < 992) ? ["nofooter", "noscroll"] : ["nofooter"]
       }
-    };
+    }
+  },
+  computed: {
+      isMobile() {
+          return window.innerWidth < 992
+      },
+      isTablet() {
+          return window.innerWidth < 1200
+      }
   }
-};
+}
 </script>
