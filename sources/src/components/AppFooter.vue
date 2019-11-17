8937bf430739cfe4b97bdd44d0eb26b5846ad3ac
Sami Kolari <sami.kolari@siilim180518.local>
2019-11-18 00:09:27 +0200

    add link to privacy policy

--- sources/src/components/AppFooter.vue
+++ sources/src/components/AppFooter.vue
@@ -2,6 +2,7 @@
  <!-- Footer -->
     <footer>
           <h2 style="text-align:center;" class="row-header">HELLO WORLD</h2>
+          <a style="position: absolute; right: 1rem; color: #111;" href="https://robotframework.org/#/privacypolicy" target="_blank">Privacy Policy</a>
           <!--<div class="slogan"><a href="http://robotframework.org/" target="_blank">robotframework.org</a></div>-->
     </footer>
 
