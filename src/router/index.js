c73a7b287bc6f16e8524fc629e948b7e2516c41e
Sami Kolari <sami_kolari@hotmail.com>
2022-04-01 10:25:05 +0300

    fix /test routing

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/test',
+    name: 'Home',
+    component: Home
+  },
+  {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: NotFound
