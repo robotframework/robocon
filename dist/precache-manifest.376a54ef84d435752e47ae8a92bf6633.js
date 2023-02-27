3f3236fd713f3460a7051da386b669146daa38bf
René <snooz@posteo.de>
2023-02-27 11:23:27 +0000

    added talks

---
+++ dist/precache-manifest.376a54ef84d435752e47ae8a92bf6633.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e3edb53349945387e617abbd0fd231",
+    "url": "dist/404.html"
+  },
+  {
+    "revision": "606eba5410640ecdb80e",
+    "url": "dist/css/app.8249a3c3.css"
+  },
+  {
+    "revision": "27679e61bfd27e1093f4bf200900b8e7",
+    "url": "dist/fonts/CourierCode-Bold.woff"
+  },
+  {
+    "revision": "bb0ae79836b57c757266e71c14e202b3",
+    "url": "dist/fonts/CourierCode-Italic.woff"
+  },
+  {
+    "revision": "b0e06d282a210d90e551bcd4d5caa8f8",
+    "url": "dist/fonts/CourierCode-Roman.woff"
+  },
+  {
+    "revision": "944290f83b2b9c22fd6ff121a8d5e012",
+    "url": "dist/fonts/CourierCode-Roman.woff2"
+  },
+  {
+    "revision": "2b7be84934e40d5204ebcd3cd1e0f4dc",
+    "url": "dist/fonts/OCRA.woff"
+  },
+  {
+    "revision": "3e146000bd230fbe73e1a3f7e9b65db5",
+    "url": "dist/fonts/OCRA.woff2"
+  },
+  {
+    "revision": "9ed7d40451046fddd70c1697a0dccc08",
+    "url": "dist/fonts/RBCN-thin.woff2"
+  },
+  {
+    "revision": "f1198aadf6009ce13452f0d244e056e7",
+    "url": "dist/fonts/RBCN22.ttf"
+  },
+  {
+    "revision": "79b0e047dd7682d0b75867ce98210e2a",
+    "url": "dist/fonts/RBCN23.otf"
+  },
+  {
+    "revision": "eb6deda9a36f356003cce94b615e2ac0",
+    "url": "dist/fonts/RBCN23.woff2"
+  },
+  {
+    "revision": "549f277743ad4c60e5503c663422c352",
+    "url": "dist/fonts/RBTFNT.ttf"
+  },
+  {
+    "revision": "63ef3191b86632e0c74a093ecad7af10",
+    "url": "dist/fonts/RBTFNT.woff"
+  },
+  {
+    "revision": "8297cf2cf2a9fc56bd612112f636de30",
+    "url": "dist/img/Calendar.svg"
+  },
+  {
+    "revision": "9dbc79dab48422c821c75bbfc21165ec",
+    "url": "dist/img/Gmail.svg"
+  },
+  {
+    "revision": "e7f37df8722d7d944bad0361d1d36f7e",
+    "url": "dist/img/RBCN22.svg"
+  },
+  {
+    "revision": "5f61873d6ea9fe47b23c09528ee13efd",
+    "url": "dist/img/RF-white.svg"
+  },
+  {
+    "revision": "719774271ca4c3ae003b0d126dbe61b9",
+    "url": "dist/img/RF.svg"
+  },
+  {
+    "revision": "f5fe2f626289d9e83ecc84a66537c631",
+    "url": "dist/img/Slack.svg"
+  },
+  {
+    "revision": "042d34e221ae14f250597bdb8eb83e11",
+    "url": "dist/img/mkorpela.jpg"
+  },
+  {
+    "revision": "3b08b66e439f376aa031338388b3ff2d",
+    "url": "dist/img/rf-pattern.jpg"
+  },
+  {
+    "revision": "27375a1648f75404801a08517a403d82",
+    "url": "dist/img/social_image.png"
+  },
+  {
+    "revision": "f4af51677bb5fda0e9df64bb3da9dd37",
+    "url": "dist/img/speaker_img_placeholder.jpg"
+  },
+  {
+    "revision": "332f97ec41198d87a751290b84294079",
+    "url": "dist/img/sponsors/camunda.png"
+  },
+  {
+    "revision": "4db95bb37bd89dc9a645095b406c0320",
+    "url": "dist/img/sponsors/capgemini.png"
+  },
+  {
+    "revision": "f249f06c58c67fa07b3c13e751d3e9d4",
+    "url": "dist/img/sponsors/cinia.png"
+  },
+  {
+    "revision": "033dd7a81cb4b42d26886d5d068ab4f3",
+    "url": "dist/img/sponsors/continero.png"
+  },
+  {
+    "revision": "d33d2dcf6f2308dfe1ac833c44f006dd",
+    "url": "dist/img/sponsors/copado.png"
+  },
+  {
+    "revision": "cb918b98a01b5c04d972371dd38a0a38",
+    "url": "dist/img/sponsors/dbschenker.png"
+  },
+  {
+    "revision": "1e004b65a52757ec27cab7f1a869da98",
+    "url": "dist/img/sponsors/eficode.png"
+  },
+  {
+    "revision": "a0795e49c42d0d50e31e2049169b779e",
+    "url": "dist/img/sponsors/fsecure.png"
+  },
+  {
+    "revision": "4b23aeb2e6128f6aea7a3c9c3ff09e64",
+    "url": "dist/img/sponsors/gofore.png"
+  },
+  {
+    "revision": "2122686b92342947561b4abf9022afe2",
+    "url": "dist/img/sponsors/imbus.png"
+  },
+  {
+    "revision": "675189b9812998be351b24f70aa9b012",
+    "url": "dist/img/sponsors/knowit.png"
+  },
+  {
+    "revision": "f3d2fd38d23fc81b632b212b6f4e52ec",
+    "url": "dist/img/sponsors/northcode.png"
+  },
+  {
+    "revision": "89d788184d19ee4d5bbcfcfd36864053",
+    "url": "dist/img/sponsors/qfactory.png"
+  },
+  {
+    "revision": "77171768148f1e7b59db35aeddb3b6f4",
+    "url": "dist/img/sponsors/reaktor.png"
+  },
+  {
+    "revision": "0113381e6e4265600e6c38ed8fec25ac",
+    "url": "dist/img/sponsors/relex.png"
+  },
+  {
+    "revision": "1e5760de77cf6281f9c5ab61e27ad262",
+    "url": "dist/img/sponsors/robocorp.png"
+  },
+  {
+    "revision": "3b87011c64f21b0acdbc3f2b63f716dd",
+    "url": "dist/img/sponsors/siili.png"
+  },
+  {
+    "revision": "ce06e24ab99fa046117346b7018f2c03",
+    "url": "dist/img/sponsors/testimate.png"
+  },
+  {
+    "revision": "aaebdc5a67663c43f9f0bd648cd8f7c3",
+    "url": "dist/img/sponsors/vala.png"
+  },
+  {
+    "revision": "77f87e86d6f721c6b5f81a87fd16c267",
+    "url": "dist/img/ticket-depth.jpg"
+  },
+  {
+    "revision": "c888e8d232a66f0aaf32046bca31320f",
+    "url": "dist/index.html"
+  },
+  {
+    "revision": "606eba5410640ecdb80e",
+    "url": "dist/js/app.1e22bdb1.js"
+  },
+  {
+    "revision": "996cb3d64e75f17bdab7",
+    "url": "dist/js/chunk-vendors.8e88fedb.js"
+  },
+  {
+    "revision": "ae69c76f0db87375aa82706bd2ce675f",
+    "url": "dist/log.html"
+  },
+  {
+    "revision": "a0ca7123d147fae4ec52a28fd583347f",
+    "url": "dist/manifest.json"
+  },
+  {
+    "revision": "086a76b11f1997cdce31cdb5d5cf262d",
+    "url": "dist/robot-framework-open-space.ics"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "dist/robots.txt"
+  }
+]);
