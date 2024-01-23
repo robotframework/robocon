9d8636d1e612be232dc1e46109a712f25b297c42
Mateusz Nojek <matnojek@gmail.com>
2024-01-23 09:26:54 +0100

    Fix path error

--- src/components/index.js
+++ src/components/index.js
@@ -17,7 +17,7 @@ import Tutorials24 from './Tutorials24.vue'
 
 import BaseIcon from './BaseIcon.vue'
 import GlobeIcon from './icons/GlobeIcon.vue'
-import GlobeRbcn from './icons/GlobeRbcn.vue'
+import GlobeRbcn from './icons/GlobeRBCN.vue'
 import LinkIcon from './icons/LinkIcon.vue'
 import ChevronIcon from './icons/ChevronIcon.vue'
 import NewTabIcon from './icons/NewTabIcon.vue'
