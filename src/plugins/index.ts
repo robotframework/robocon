e612ae11fa61b00f2c1d1a1f1c14142a424cdf9a
jalyn <fleecycloud215@gmail.com>
2024-08-22 21:40:23 +0900

    style: update color

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,14 +1,6 @@
-/**
- * plugins/index.ts
- *
- * Automatically included in `./src/main.ts`
- */
 
-// Plugins
 import vuetify from './vuetify'
 import router from '../router'
-
-// Types
 import type { App } from 'vue'
 
 export function registerPlugins(app: App) {
