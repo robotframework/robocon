592930fcbff6c24436173c066c8e536c2ce0b6b7
jalyn <fleecycloud215@gmail.com>
2024-09-01 15:05:19 +0900

    migrate: static -> contentful template rendering

--- src/components/footer/PageFooter.vue
+++ src/components/footer/PageFooter.vue
@@ -1,118 +1,44 @@
 <template>
-  <v-footer class="">
-    <v-container fluid style="line-height: 2;">
-      <v-row>
-        <v-col class="col-sm-6 col-md-3">
-          <h3 class="mb-small">
-            <a class="text-primary" href="https://robotframework.org/foundation/">Foundation</a>
-          </h3>
-          <div>
-            Robot Framework ry
-          </div>
-          <div>
-            Kampinkuja 2
-          </div>
-          <div>
-            00100 Helsinki
-          </div>
-          <div>
-            Finland
-          </div>
-          <a class="text-primary" href="mailto:robotframework-foundation@googlegroups.com">
-            Contact
-          </a>
-        </v-col>
-        <div class="col-sm-6 col-md-3" :class="isMobile ? 'type-right' : ''">
-          <h3 class="mb-small text-primary">
-            Community
-          </h3>
-          <div>
-            <a class="text-primary" href="https://github.com/robotframework/robotframework">
-              GitHub
-            </a>
-          </div>
-          <div>
-            <a class="text-primary" href="https://forum.robotframework.org/">
-              Forum
-            </a>
-          </div>
-          <div>
-            <a class="text-primary" href="https://www.facebook.com/robotframeworkofficial">
-              Facebook
-            </a>
-          </div>
-          <div>
-            <a class="text-primary" href="https://twitter.com/robotframework">
-              Twitter
-            </a>
-          </div>
-          <div>
-            <a class="text-primary" href="https://www.linkedin.com/groups/3710899/">
-              LinkedIn
-            </a>
-          </div>
-        </div>
-        <div v-if="isMobile" class="col-sm-12 mb-large" />
-        <div class="col-sm-6 col-md-3">
-        </div>
-        <div class="col-sm-6 col-md-3 flex flex-col between">
-          <div />
-          <div :class="isMobile ? 'type-right' : ''">
-            <div>
-              <a class="text-primary" href="https://robotframework.org">
-                Robot Framework
-              </a>
+  <template v-if="store.getFooter">
+    <v-footer color="primary">
+      <v-container fluid style="line-height: 2">
+        <v-row>
+          <v-col v-for="(footer) in store.getFooter" class="col-sm-6 col-md-3 ga-3">
+            <div :class="[index > 0 ? 'mt-2' : '', 'd-flex flex-column']" v-for="(item, index) in footer">
+              <template v-if="item?.address">
+                <div class="address">
+                  {{ item.address }}
+                </div>
+              </template>
+              <template v-else>
+                <a :href="item.href" target="_blank" class="title-font">{{ item.name }} </a>
+              </template>
             </div>
-            <div>
-              <a class="text-primary" href="https://robotframework.org/privacy-policy">
-                Privacy Policy
-              </a>
-            </div>
-          </div>
-        </div>
-      </v-row>
-    </v-container>
-  </v-footer>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-footer>
+  </template>
 </template>
 
-<script>
-// import GlobeIcon from './icons/GlobeIcon.vue'
-import { useGlobalStore } from '@/store';
-import { mapState } from 'pinia';
 
-export default {
-  name: 'PageFooter',
-  components: {
-    // GlobeIcon
-  },
-  data: () => ({
-    langDropdownOpen: false
-  }),
-  computed: {
-    langNames() {
-      return Object.keys(this.$i18n.messages)
-        .map((lang) => ({ lang, name: this.$i18n.messages[lang].langName }))
-        .filter(({ name }) => name !== 'translation')
-    },
-    ...mapState(useGlobalStore, ['isMobile']),
-  },
-  methods: {
-    setLang(lang) {
-      this.$i18n.locale = lang
-      window.localStorage.setItem('lang', lang)
-    }
-  }
-}
+<script setup>
+import { useGlobalStore } from '@/store/modules';
+const store = useGlobalStore();
 </script>
 
-<style scoped>
-@media screen and (max-width: 768px) {
-  div {
-    font-size: var(--type-small);
-  }
+<style scoped lang="scss">
+a {
+  color: rgb(var(--v-theme-white));
+  font-weight: 600;
 
-  h3 {
-    font-size: var(--type-body);
+  &:hover {
+    opacity: 0.8;
   }
+}
+
+.address {
+  white-space: pre-line;
+  opacity: 0.8;
 }
 </style>
