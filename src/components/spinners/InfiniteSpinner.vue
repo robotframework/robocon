26a5a360caa83380bc28989c03f30b82c3b40729
jalyn <fleecycloud215@gmail.com>
2024-09-01 15:02:36 +0900

    feat: add infinite spinner component

---
+++ src/components/spinners/InfiniteSpinner.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-progress-circular :width="props.width" :size="props.size" :color="props.color" :indeterminate="indeterminate"
+    class="text-center">
+    <template v-slot:default> {{ loadingText }} </template>
+  </v-progress-circular>
+</template>
+
+
+<script setup lang="ts">
+
+const props = defineProps({
+  loadingText: {
+    type: String,
+    default: ''
+  },
+  color: {
+    type: String,
+    default: "primary"
+  },
+  width: {
+    type: Number,
+    default: 3
+  },
+  size: {
+    type: Number,
+    default: 10
+  },
+  indeterminate: {
+    type: Boolean,
+    default: true
+  },
+})
+</script>
