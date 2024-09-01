28b417cc6f87de50d37d6e3c82395d6a10994e8d
jalyn <fleecycloud215@gmail.com>
2024-09-01 15:09:25 +0900

    chore: modify file name casing

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,21 @@
 <template>
   <v-app full-height>
     <v-main class="text-slate-800 dark:text-slate-200 pt-16">
-      <navbar />
+      <Navbar />
       <router-view />
-
-      <page-footer v-if="!fullScreen" />
+      <PageFooter />
     </v-main>
   </v-app>
 </template>
 
-<script>
-import { Navbar, PageFooter } from 'Components'
+<script setup>
+import { Navbar, PageFooter } from "Components";
+import { watch } from "vue";
+import { useI18n } from "vue-i18n";
 
-export default {
-  components: {
-    Navbar,
-    PageFooter
-  },
-  data: () => ({
-    fullScreen: false
-  }),
-  created() {
-    window.addEventListener('click', () => document.body.classList.remove('accessible'))
-    window.addEventListener('keydown', ({ key }) => {
-      if (key === 'Tab') { document.body.classList.add('accessible') }
-    })
-    const params = new URLSearchParams(window.location.search)
-    const attendee = Object.fromEntries(params.entries()).attendee
-    if (attendee === 'gather') this.fullScreen = true
-  },
-  watch: {
-    '$i18n.locale'() {
-      document.documentElement.lang = this.$i18n.locale
-    }
-  },
-}
+const { t, locale } = useI18n();
+
+watch(locale, () => {
+  document.documentElement.lang = locale.value
+});
 </script>
