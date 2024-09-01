7c76527f935fc2cb351e289ca3b154e2b140604f
jalyn <fleecycloud215@gmail.com>
2024-09-01 14:43:46 +0900

    feat: add main sections components

---
+++ src/views/main/TicketSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <page-section :data="store.getMainTickets2025" :activeView="activeView" :isResponsiveContainer="false" />
+</template>
+
+<script setup lang="ts">
+import { useGlobalStore } from '@/store/modules';
+import { ref } from 'vue';
+
+const activeView = ref("in_person_card_2025")
+const store = useGlobalStore()
+
+</script>
+
+<style scoped lang="scss">
+.slider-row {
+  overflow: hidden;
+}
+</style>
