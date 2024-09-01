57dc9dcce3508639a1c4aaebb7dd7eec44014c3d
jalyn <fleecycloud215@gmail.com>
2024-09-01 15:10:01 +0900

    enhance: update genentries for contentful

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store, { useGlobalStore } from './store'
-import { createI18n } from 'vue-i18n'
-import { english, german, getEntries } from './content'
-import 'Css/index.css'
-import vuetify from './plugins/vuetify';
-import type { BaseEntry, Entry, EntrySkeletonType } from 'contentful'
+import { Entry } from "contentful";
+import "Css/index.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { getEntries } from "./content";
+import { i18nConfig } from "./i18n";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import store from "./store";
+import { useGlobalStore } from "./store/modules";
 
-const i18n = createI18n({
-  locale: 'en-US',
-  messages: {
-    'en-US': { ...english },
-    'de-DE': { ...german }
-  },
-  warnHtmlInMessage: 'off'
+const app = createApp(App)
+
+app.use(store).use(i18nConfig).use(vuetify)
+
+getEntries().then((res: unknown) => {
+  const pages = (res as Array<Entry>)?.filter(
+    ({ sys }) => sys.contentType.sys.id === "page",
+  );
+
+  const st = useGlobalStore()
+  st.setPages(pages);
+  app.use(router).mount("#app");
 })
 
-const app = createApp(App).use(store).use(i18n).use(vuetify)
 
-getEntries()
-  .then((res: unknown) => {
-    const pages = (res as Array<Entry<EntrySkeletonType<BaseEntry>>>)?.filter(({ sys }) => sys.contentType.sys.id === 'page')
-
-    const st = useGlobalStore()
-    st.updatePages(pages)
-
-    // st.$patch({ pages })
-    app.use(router)
-    app.mount('#app')
-  })
