3014a67940b02ef398e76bbf3203aa5c7e26f5f0
samipe <sami_kolari@hotmail.com>
2024-09-02 18:30:12 +0300

    vite outdir

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './docs',
+    emptyOutDir: true,
+  },
   plugins: [
     vue({
       template: { transformAssetUrls },
