05e33d82deb33fe4af44b11a35ed8d8a193502ee
samipe <sami_kolari@hotmail.com>
2025-03-02 18:24:40 +0200

    stream buttons

--- src/views/Stream.vue
+++ src/views/Stream.vue
@@ -5,16 +5,15 @@
     </h1>
     <div v-else class="px-small py-xsmall row between">
       <div>
-        <button class="theme mr-small" :class="selectedDay === 1 && 'active'" @click="selectedDay = 1">Day 1</button>
-        <button class="theme" :class="selectedDay === 2 && 'active'" @click="selectedDay = 2">Day 2</button>
+        <button v-for="(stream, key) in streams" :key="stream.name" class="theme mr-small" :class="selectedDay === key && 'active'" @click="selectedDay = key">{{ stream.name }}</button>
       </div>
       <button @click="showChat = !showChat" class="theme small type-small">
         {{ showChat ? 'Hide Q&amp;A' : 'Show Q&amp;A' }}
       </button>
     </div>
     <div class="stream-container" :class="store.isFullscreen && 'fullscreen'">
-      <iframe class="stream col-sm-12" :class="showChat && 'col-md-9'" :src=streamUrl title="Robocon stream" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <iframe v-show="showChat" class="chat col-sm-12 col-md-3" :src=chatUrl frameBorder="0" title="Stream chat"></iframe>
+      <iframe class="stream col-sm-12" :class="showChat && 'col-md-9'" :src=urls.stream title="Robocon stream" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe v-show="showChat" class="chat col-sm-12 col-md-3" :src=urls.chat frameBorder="0" title="Stream chat"></iframe>
     </div>
   </div>
   <div v-if="!store.isFullscreen" class="container narrow">
@@ -25,24 +24,28 @@
 <script setup lang="ts">
 import PretalxSchedule from 'Components/PretalxSchedule.vue';
 import { useStore } from 'Store';
-import { decrypt, getVideoUrl, initAuth } from 'Utils/ticket';
-import { ref, onMounted } from 'vue';
+import { decrypt, getVideoUrl } from 'Utils/ticket';
+import { ref, computed } from 'vue';
 
 const store = useStore()
 
 const chat = 'U2FsdGVkX1+gSb33tfz6U6doTSeJvDfWihHORmgrcBmJTBILeyQlaBmAYh4gZmxksEH+fL5NzUrv5sFyk43D/w=='
+const streams = [
+  { name: 'Day 1', id: 'day1' },
+  { name: 'Day 2', id: 'day2' }
+]
 
-const selectedDay = ref(1)
+const selectedDay = ref(0)
 const showChat = ref(true)
-const streamUrl = ref('')
-const chatUrl = ref('')
-const error = ref(false)
 
-onMounted(async () => {
-  await initAuth()
-  streamUrl.value = await getVideoUrl(`day${selectedDay.value}`, true)
-  console.log(streamUrl.value)
-  chatUrl.value = await decrypt(chat)
+const urls = computed(() => {
+  const store = useStore()
+  if (!store.token) return { stream: undefined, chat: undefined }
+  const { id } = streams[selectedDay.value]
+  return {
+    stream: getVideoUrl(id, true),
+    chat: decrypt(chat)
+  }
 })
 
 </script>
